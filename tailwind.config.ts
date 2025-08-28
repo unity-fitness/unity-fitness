3b8c89c5e4aca6102c6da3ec4075ee916aa53294
unity-fitness <samuelalvesgeo@gmail.com>
2025-08-28 12:53:22 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: { offwhite: "#F7F7F9" }
-    },
-  },
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: { colors: { offwhite: "#F7F7F9" } } },
   plugins: [],
 } satisfies Config;
